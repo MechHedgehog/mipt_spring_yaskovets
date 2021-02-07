290201b59eb356b71d71189d6ef25ab05eee7ae5
MechHedgehog <71868148+MechHedgehog@users.noreply.github.com>
2021-02-08 00:08:23 +0300

    Add files via upload

---
+++ hw_02.08/task6.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n / 2 + n % 2; i++) cout << string(i, ' ') + string(n - i * 2, '*') << endl;
+	return 0;
+}
